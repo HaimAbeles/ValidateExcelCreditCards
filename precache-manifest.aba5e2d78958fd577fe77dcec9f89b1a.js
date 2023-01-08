67ee9f3823659c07da89c822c40b9642cb29e048
CHAIM-LAP\1 <a7112079@gmail.com>
2023-01-09 01:06:24 +0200

    Updates

---
+++ precache-manifest.aba5e2d78958fd577fe77dcec9f89b1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4a94e23670a112015deb5c7623c585b",
+    "url": "/ValidateExcelCreditCards/index.html"
+  },
+  {
+    "revision": "f270c340e26440544cb0",
+    "url": "/ValidateExcelCreditCards/static/css/main.3ca16d9b.chunk.css"
+  },
+  {
+    "revision": "ddd09d60d5fdd7c7afd1",
+    "url": "/ValidateExcelCreditCards/static/js/2.ef735424.chunk.js"
+  },
+  {
+    "revision": "9ac7d1fb223f2f9a1e9e",
+    "url": "/ValidateExcelCreditCards/static/js/3.79a04dbb.chunk.js"
+  },
+  {
+    "revision": "f270c340e26440544cb0",
+    "url": "/ValidateExcelCreditCards/static/js/main.e03c1ac4.chunk.js"
+  },
+  {
+    "revision": "7af2b34123d5b0381433",
+    "url": "/ValidateExcelCreditCards/static/js/runtime~main.ec962fc2.js"
+  }
+]);
